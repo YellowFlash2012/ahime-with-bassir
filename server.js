70447db7f0b1ad9eba0ba284cd8905944d1e9c37
YellowFlash2012 <cyclop9@hotmail.com>
2022-12-28 08:46:00 +0100

    setup basic express server and rendered the product data from there

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express"
+import colors from "colors"
+import { config } from "dotenv"
+import morgan from "morgan"
+import helmet from "helmet"
+
+import data from "./database/data.js"
+
+
+
+config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(helmet())
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+app.get('/', (req, res) => {
+    res.send(data.products)
+})
+
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode | Port ${port}`.yellow.bold);
+})
